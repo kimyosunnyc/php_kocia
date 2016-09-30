61c1290b37f1bded53f232999a9bfeb6d161ca96
kimyosunnyc <kimyosunnyc@gmail.com>
2016-09-30 14:06:43 +0900

    윈도우 언어 설정에 따른 리다이렉트

---
+++ html/exercise/test/redirect_test.php
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+	var userLang = navigator.language || navigator.userLanguage; 
+	//alert ("The language is: " + userLang);
+	window.location.href='http://www.naver.com';
+</script>
